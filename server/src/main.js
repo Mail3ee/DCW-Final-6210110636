35a7ec3a49299a48cf0742951f285284b7ccbe73
ไกรวิชญ์ เสนาสุ <jate2521@gmail.com>
2022-03-15 04:19:55 +0700

    use google auth and axios

--- server/src/main.js
+++ server/src/main.js
@@ -1,16 +1,21 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const port = 8080
+const axios = require('axios')
+const cors = require('cors')
+const port = 8000
 const app = express()
 
 app.use(bodyParser.json());
+app.use(cors())
 
 app.get('/', (req, res) =>{
     res.send('Hello World')
 })
 
 app.post('/login', (req, res) =>{
-    res.send('Login Now')
+    let access_token = req.body.token
+    let _profile = req.body.profile
+    res.send({token: access_token, profile: _profile})
 })
 
 
--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,60 @@
-import logo from './logo.svg';
 import './App.css';
+import GoogleLogin from 'react-google-login'
+import { useState } from 'react'
+import axios from 'axios'
 
 function App() {
+
+    const [loginData, setLoginData] = useState(
+        localStorage.getItem('loginData') 
+        ? JSON.parse(localStorage.getItem('loginData'))
+        :null     
+    )
+
+  const handleFailure = (result) =>{
+    console.log(result)
+  }
+  const handleLogin = async (response) =>{
+        if (response.accessToken) {
+            let result = await axios.post('http://localhost:8000/login', {
+                token : response.accessToken,
+                profile : response.profileObj
+            })
+            console.log(result)
+            setLoginData(result.data.profile)
+        }
+  }
+
+  const handleLogout = () =>{
+    localStorage.removeItem('loginData')
+    setLoginData(null)
+}
+
+  
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Google Login App</h1>
+        <div>
+            {
+                loginData ? (
+                    <div>
+                        <img src = {loginData.imageUrl}></img>
+                        <h3>You logged in as {loginData.name}</h3>
+
+                        <button onClick={handleLogout}>Logout</button>
+                    </div>
+                ):(
+                    <GoogleLogin 
+                    clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+                    buttonText = "Login with Google"
+                    onSuccess={handleLogin}
+                    onFailure={handleFailure}
+                    cookiePolicy={'single_host_origin'}>
+                  </GoogleLogin>
+                )
+            }
+        </div>
       </header>
     </div>
   );
