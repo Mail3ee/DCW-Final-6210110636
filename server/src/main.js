1f3e553a4f5cfdd26e64e58d8409ada9a8daea49
ไกรวิชญ์ เสนาสุ <jate2521@gmail.com>
2022-03-14 18:13:17 +0700

    add login path

--- server/src/main.js
+++ server/src/main.js
@@ -1,10 +1,19 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 const port = 8080
 const app = express()
+
+app.use(bodyParser.json());
 
 app.get('/', (req, res) =>{
     res.send('Hello World')
 })
+
+app.post('/login', (req, res) =>{
+    res.send('Login Now')
+})
+
+
 
 app.listen(port, ()=>{
     console.log(`Listening on port ${port}`)
